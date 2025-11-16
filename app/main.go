161c869447b2ee12e6cfffb93f0d3dfca505d53d
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2025-11-16 01:55:49 +0100

    add status based REPL exit

--- app/main.go
+++ app/main.go
@@ -4,17 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		commandLn, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		command := commandLn[:len(commandLn)-1]
+
+		cmdParts := strings.Split(command, " ")
+		if len(cmdParts) > 1 && cmdParts[0] == "exit" {
+			n, _ := strconv.Atoi(cmdParts[1])
+			os.Exit(n)
+		}
+
+		fmt.Println(command + ": command not found")
 	}
 }
