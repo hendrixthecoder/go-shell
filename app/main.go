efd7e852c24f7f4af2d6a123d2369b3b10dac941
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2025-11-16 10:37:57 +0100

    add type builtin

--- app/main.go
+++ app/main.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+var BUILTIN = []string{"type", "echo", "exit"}
 
 func main() {
 	for {
@@ -20,23 +23,49 @@ func main() {
 		command := commandLn[:len(commandLn)-1]
 
 		cmdParts := strings.Split(command, " ")
-		if len(cmdParts) < 2 {
-			fmt.Println(command + ": command not found")
-			continue
-		}
 
 		switch cmdParts[0] {
 		case "exit":
-			n, _ := strconv.Atoi(cmdParts[1])
-			os.Exit(n)
-		case "echo":
-			args := make([]any, len(cmdParts[1:]))
+			if len(cmdParts) < 2 {
+				fmt.Println("exit: requires an argument.")
+				continue
+			}
 
+			n, err := strconv.Atoi(cmdParts[1])
+			if err != nil {
+				fmt.Println("exit: invalid exit code.")
+				continue
+			}
+
+			os.Exit(n)
+
+		case "echo":
+			if len(cmdParts) < 2 {
+				fmt.Println()
+				continue
+			}
+
+			args := make([]any, len(cmdParts[1:]))
 			for i, v := range cmdParts[1:] {
 				args[i] = v
 			}
 
 			fmt.Fprintln(os.Stdout, args...)
+
+		case "type":
+			if len(cmdParts) < 2 {
+				fmt.Println("type: requires an argument.")
+				continue
+			}
+
+			target := cmdParts[1]
+
+			if slices.Contains(BUILTIN, target) {
+				fmt.Println(target + " is a shell builtin")
+			} else {
+				fmt.Println(target + ": not found")
+			}
+
 		default:
 			fmt.Println(cmdParts[0] + ": command not found")
 		}
