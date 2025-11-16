d4f6c89f0b864a79b189c6754b26940347205561
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2025-11-16 01:25:29 +0100

    empty

--- app/main.go
+++ app/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
